f2aacacf83c7e831b29189109892ac9bc861c9b3
Usborn Ocampo <usborn116@gmail.com>
2023-10-03 13:29:51 -0700

    add 0 score for each new player added to a session, in each category

--- app/models/session_player.rb
+++ app/models/session_player.rb
@@ -1,9 +1,20 @@
 class SessionPlayer < ApplicationRecord
     belongs_to :session
     has_many :session_scores
+
+    after_create :create_scores
     
     def total_score
         self.session_scores.map(&:amount).sum
+    end
+
+    private
+
+    def create_scores
+        session_id = self.session.id
+        self.session.session_categories.each do |c|
+            self.session_scores.create(amount: 0, session_id: session_id, session_category_id: c.id)
+        end
     end
 
 
--- spec/models/session_player_spec.rb
+++ spec/models/session_player_spec.rb
@@ -5,22 +5,34 @@ RSpec.describe SessionPlayer, type: :model do
     @user = User.last || User.create(name:'Usertest', email: 'user_email@email.com', password:'userpassword')
     @user.save!
     @game = @user.games.create
+    @cat1 = @game.categories.create(name: 'Cat 1')
+    @cat2 = @game.categories.create(name: 'Cat 2')
     @sesh = @game.sessions.create
     @player1 = @sesh.session_players.create(name: 'Usborn')
     @player2 = @sesh.session_players.create(name: 'Ashley')
-    @player1.session_scores.create(amount: 4)
-    @player1.session_scores.create(amount: 6)
-    @player2.session_scores.create(amount: 4)
-    @player2.session_scores.create(amount: 3)
   end
 
-  context 'after all scores are entered' do
+  context 'after each session player is created' do
+    it 'creates a 0 score for each category' do
+      expect(@player1.total_score).to eq(0)
+      expect(@player2.total_score).to eq(0)
+      expect(@sesh.session_scores.length).to eq(4)
+      expect(@player1.session_scores.length).to eq(2)
+      expect(@player2.session_scores.length).to eq(2)
+    end
+
     it 'calculates the first players total score' do
+      @player1.session_scores.first.update(amount: 4)
+      @player1.session_scores.last.update(amount: 6)
       expect(@player1.total_score).to eq(10)
     end
 
     it 'calculates the second players total score' do
-      expect(@player2.total_score).to eq(7)
+      @player2.session_scores.first.update(amount: 4)
+      @player2.session_scores.last.update(amount: 3)
+      expect(@player2.total_score).to eq(7) 
     end
+
   end
+
 end
