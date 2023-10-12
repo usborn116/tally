dbb057c9a9fdd3aeb9262bc0f27b3ab15a80a2ee
Usborn Ocampo <usborn116@gmail.com>
2023-10-12 15:21:49 -0700

    added footer

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -7,8 +7,8 @@ export default App = () => {
     const {user, setUser, loading, setLoading, error, setError} = useSetUser()
 
     return (
-        <>
+        <div className="main">
             <RoutesIndex user={user} setUser={setUser} loading={loading} error={error} setError={setError} setLoading={setLoading}/>
-        </>
+        </div>
     )
 }
