ca60516519dd8c4a0bae492d90cdf12c29dc8174
Usborn Ocampo <usborn116@gmail.com>
2023-10-20 15:40:15 -0700

    styled session component

--- app/javascript/components/Session.jsx
+++ app/javascript/components/Session.jsx
@@ -13,15 +13,9 @@ import { Error } from "./Error";
 
 export const Session = () => {
 
-    const navigate = useNavigate()
-
     const {error, setError} = useError()
 
     const id = useParams().id
-
-    const {user} = useSetUser()
-
-    //if (!user) navigate('/login')
 
     const [data, setData] = useState([])
     const [numPlayers, setNumPlayers] = useState(0)
@@ -95,49 +89,65 @@ export const Session = () => {
     if (error) return <Error message={error}/>
 
     return (
-        <>
-            <Button handler={() => navigate(-1)}>Back</Button>
-            <Switcher setter={setCreate} data={create}>{create ? 'Done Adding' : 'Add New Player'}</Switcher>
-            {create ? 
-            <Form endpoint="players" item='player' updater={newData} setter={setData} setToggle={setCreate} setError={setError}>
-                <Input type="text" name="name" value={data.name}/>
-                <Submit>Save</Submit>
-            </Form> : ''}
-            <Switcher setter={setAddPlayers} data={addPlayers}>{addPlayers ? 'Done Adding' : 'Add Player to Game'}</Switcher>
-            {addPlayers ?
-            <>
-                <h3>How many players are you adding?</h3>
-                <input type="number" defaultValue={numPlayers} onChange={handleChange}></input>
-                {add}
-            </>
-             : ''}
-            <div>{data?.session?.game?.name}</div>
-            <Switcher setter={setEditDate} data={editDate}>Change Date</Switcher>
-            {editDate ? 
-            <Form endpoint="sessions" item='session' id={data?.session?.id} updater={updateData} 
-                setter={setData} setToggle={setEditDate} setError={setError}>
-                <Input type="date" name="date" value={data?.session?.date}/>
-                <Submit>Save</Submit>
-            </Form> :  <div>{data?.session?.date}</div>}
-            <h3>Players</h3>
-            {players}
-            <h3>Categories</h3>
-            {categories}
-            <h3>Scores</h3>
-            <Switcher setter={setEnterScores} data={enterScores}>Enter Scores</Switcher>
-            <div className="table">
-                <div className="headers" 
-                    style={{gridTemplateColumns: `repeat(${data?.session?.session_players?.length + 1}, 1fr)`}}
-                >
-                    <div></div>
-                    {players}
+        <div className="table">
+            <div className="top-session">
+                <h2>{data?.session?.game?.name}</h2>
+                <div className="entry date-entry">
+                    <h3>{editDate ? 
+                        <Form endpoint="sessions" item='session' id={data?.session?.id} updater={updateData} 
+                            setter={setData} setToggle={setEditDate} setError={setError}>
+                            <Input type="date" name="date" value={data?.session?.date}/>
+                            <Submit>Save</Submit>
+                        </Form> :  data?.session?.date}
+                    </h3>
+                    <Switcher setter={setEditDate} data={editDate}>Change Date</Switcher>
                 </div>
-            {scores2}
-            {totals}
+                <h3>Winner: {data?.session?.victor || 'None Yet'}</h3>
             </div>
-            <Button endpoint={`/session_winner/${data?.session?.id}`} setData={setData} handler={handleCalculate}>Calculate Score</Button>
-            <div>Winner: {data?.session?.victor}</div>
-        </>
+
+            <div className="session-data">
+
+                <div className="player-section game-details">
+                    <h3>Players</h3>
+                    {players}
+                    <br></br>
+                    <Switcher setter={setCreate} data={create}>{create ? 'Done Adding' : '+ Player to Account'}</Switcher>
+                    {create ? 
+                    <Form endpoint="players" item='player' updater={newData} setter={setData} setToggle={setCreate} setError={setError}>
+                        <Input type="text" name="name" value={data.name}/>
+                        <Submit >Save</Submit>
+                    </Form> : ''}
+                    <Switcher setter={setAddPlayers} data={addPlayers}>{addPlayers ? 'Done Adding' : '+ Player(s) to Game'}</Switcher>
+                    <br></br>
+                    {addPlayers ?
+                    <div className="data">
+                        <div># Players</div>
+                        <input type="number" defaultValue={numPlayers} onChange={handleChange}></input>
+                        {add}
+                    </div>
+                    : ''}
+                </div>
+                
+                <div className="scores game-details">
+                    <div className="game-data">
+                        <h3>Scores</h3>
+                        <Switcher setter={setEnterScores} data={enterScores}>+ Scores</Switcher>
+                    </div>
+                    <div className="table">
+                        <div className="headers" 
+                            style={{gridTemplateColumns: `repeat(${data?.session?.session_players?.length + 1}, 1fr)`}}
+                        >
+                            <div></div>
+                            {players}
+                        </div>
+                    {scores2}
+                    {totals}
+                    </div>
+                    <Button endpoint={`/session_winner/${data?.session?.id}`} setData={setData} handler={handleCalculate}>Calculate Score</Button>
+                </div>
+
+            </div>
+        </div>
         
 
     )
