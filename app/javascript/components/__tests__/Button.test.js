b1c007bcb351643e00fa14b74d00d3c46b4ab9e7
Usborn Ocampo <usborn116@gmail.com>
2023-11-21 16:46:48 -0800

    added more tests to the Jest testing suite

--- app/javascript/components/__tests__/Button.test.js
+++ app/javascript/components/__tests__/Button.test.js
@@ -3,12 +3,10 @@
  */
 import {render, screen} from '@testing-library/react'
 import { Button } from '../Button';
-import { Home } from '../Home';
 
 
 describe('button has expected text',() => {
     test('renders correct text', () => {
-        render(<Home />)
         render(<Button/>);
         const button = screen.getByRole('button');
         expect(button.textContent).toMatch(/Create New/)
---
+++ app/javascript/components/__tests__/Home.test.js
@@ -0,0 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+import {render, screen} from '@testing-library/react'
+import { Home } from '../Home';
+
+
+describe('Home has expected first header',() => {
+    test('renders correct text for no user', () => {
+        render(<Home setLoading={() => {}} setError={() => {}}/>)
+        const heading = screen.getAllByRole('heading');
+        expect(heading[0].textContent).toMatch(/Welcome To Tally, friend/)
+    });
+
+    test('renders correct text for user with name John', () => {
+        render(<Home setLoading={() => {}} setError={() => {}} user={{name: 'John'}}/>)
+        const heading = screen.getAllByRole('heading');
+        expect(heading[0].textContent).toMatch(/Welcome To Tally, John/)
+    });
+
+    test('has the games component', () => {
+        render(<Home setLoading={() => {}} setError={() => {}}/>)
+        const heading = screen.getAllByText('All Games')
+        expect(heading[0]).toBeDefined()
+    })
+    
+    test('has the players component if user', () => {
+        render(<Home setLoading={() => {}} setError={() => {}} user={{name: 'John'}}/>)
+        expect(screen.getByText('Your Players')).toBeDefined()
+    })
+  })
