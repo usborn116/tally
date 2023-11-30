15e715e353436cd2eec3e503532611fdd2538cb2
Usborn Ocampo <usborn116@gmail.com>
2023-11-29 16:01:15 -0800

    created Login and Home tests

--- app/javascript/components/__tests__/Home.test.js
+++ app/javascript/components/__tests__/Home.test.js
@@ -3,6 +3,14 @@
  */
 import {render, screen} from '@testing-library/react'
 import { Home } from '../Home';
+import { Players } from '../Players';
+import { Player } from '../Player';
+import userEvent from '@testing-library/user-event';
+
+
+const user = userEvent.setup()
+
+const mockHomeError = jest.fn()
 
 
 describe('Home has expected components',() => {
@@ -22,18 +30,31 @@ describe('Home has expected components',() => {
         render(<Home setLoading={() => {}} setError={() => {}}/>)
         const heading = screen.getAllByText('All Games')
         expect(heading[0]).toBeDefined()
+        expect(screen.queryByText('Your Players')).toBe(null)
     })
 
     test('has the players component if user', () => {
         render(<Home setLoading={() => {}} setError={() => {}} user={{name: 'John'}}/>)
-        expect(screen.getByText('Your Players')).toBeDefined()
+        expect(screen.queryByText('Your Players')).not.toBe(null)
     })
 
-    test('you can add a new player', () => {
-        //render(<Home setLoading={() => {}} setError={() => {}} user={{name: 'John'}}/>)
+    test('you can add a new player', async () => {
+        render(<Players homeError={mockHomeError}/>)
+        await user.click(screen.getByText('Add New Player'))
+        const box = screen.getByRole('textbox')
+        expect(box).toBeDefined()
+        expect(box.value).toBe('')
+        await user.type(box, 'User1')
+        expect(box.value).toBe('User1')
+        await user.click(screen.getByText('Never mind!'))
+        expect(screen.queryByRole('textbox')).toBe(null)
     })
 
-    test('you can save changes to an existing player', () => {
-        //render(<Home setLoading={() => {}} setError={() => {}} user={{name: 'John'}}/>)
+    test('you can save changes to an existing player', async () => {
+        render(<Player key={1} data={{id: 2, name: 'John'}} setData={jest.fn()}/>)
+        const player = screen.getByRole('textbox')
+        expect(player.value).toBe('John')
+        await user.type(player, 'ny')
+        expect(player.value).toBe('Johnny')
     })
   })
