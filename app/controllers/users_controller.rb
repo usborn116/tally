0e7a8ab6a37769eda2abd3d3b752f2a40a19249a
Usborn Ocampo <usborn116@gmail.com>
2024-05-22 10:08:38 -0700

    refactored Games controller and model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!
     
     def show
-      render json: current_user.as_json(:include => [{:games => {:include => [:sessions]}}, {:sessions => {:include => [:user]}}, {:shared_sessions => {:include => [:user]}}]) || false
+      render json: current_user.as_json(:include => 
+        [ {:games => {:include => [:sessions]}}, 
+          {:sessions => {:include => [:user]}}, 
+          {:shared_sessions => {:include => [:user]}} ] ) || false
     end
 end
