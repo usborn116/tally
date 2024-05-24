4a64224d1ee3af95dfd6891375b3ae47ae80c589
Usborn Ocampo <usborn116@gmail.com>
2024-05-24 13:26:18 -0700

    added include params to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,30 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!
     
     def show
-      render json: current_user.as_json(:include => 
-        [ {:games => {:include => [:sessions]}}, 
-          {:sessions => {:include => [:user]}}, 
-          {:shared_sessions => {:include => [:user]}} ] ) || false
+
+      include_options = build_include_options
+
+      render json: current_user.as_json(include: include_options) || false
     end
+
+    private
+
+    def build_include_options
+      return {} unless params[:include]
+
+      params[:include].split(',').map do |association|
+        case association
+        when 'games'
+          { games: {include: :sessions}}
+        when 'sessions'
+          { sessions: {include: :user}}
+        when 'shared_sessions'
+          { shared_sessions: {include: :user}}
+        else
+          {}
+        end
+      end.reduce({}, :merge)
+    end
+
 
 end
